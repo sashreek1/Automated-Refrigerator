6e7824b1b9fde7071c577c388ca188e12dc09705
sashreek1 <sashreek713@gmail.com>
2019-01-31 11:54:06 +0530

    final commit

--- twilio_trial.py
+++ twilio_trial.py
@@ -1,7 +1,7 @@
 def SMS_sender(message):
 	from twilio.rest import Client
-	account_sid = 'AC09e86058bb42a4629fe4b1fe9e471266'
-	auth_token = 'e0cd2b586075f7438b27b7debfc57474'
+	account_sid = 'AC09e86058bb42a4629266'#sample api key
+	auth_token = 'e0cd2b586075f'#sample token
 	myPhone = '+919901939100' 
 	TwilioNumber = '+15853022359'
 	client = Client(account_sid, auth_token)
